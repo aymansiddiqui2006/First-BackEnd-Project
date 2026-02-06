b5b11e740259cf87e921a20df40565f23be5e729
Your Nameaymansiddiqui2006 <aymansiddiqui2006@gmail.com>
2026-02-07 01:18:18 +0530

    multer completed and coludnary completed

--- src/utility/cloudinary.js
+++ src/utility/cloudinary.js
@@ -1,42 +1,29 @@
 import { v2 as cloudinary } from 'cloudinary';
+import fs from "fs"
 
-(async function() {
 
-    // Configuration
-    cloudinary.config({ 
-        cloud_name: 'dp14tl2ox', 
-        api_key: '944637576116654', 
-        api_secret: '<your_api_secret>' // Click 'View API Keys' above to copy your API secret
-    });
-    
-    // Upload an image
-     const uploadResult = await cloudinary.uploader
-       .upload(
-           'https://res.cloudinary.com/demo/image/upload/getting-started/shoes.jpg', {
-               public_id: 'shoes',
-           }
-       )
-       .catch((error) => {
-           console.log(error);
-       });
-    
-    console.log(uploadResult);
-    
-    // Optimize delivery by resizing and applying auto-format and auto-quality
-    const optimizeUrl = cloudinary.url('shoes', {
-        fetch_format: 'auto',
-        quality: 'auto'
-    });
-    
-    console.log(optimizeUrl);
-    
-    // Transform the image: auto-crop to square aspect_ratio
-    const autoCropUrl = cloudinary.url('shoes', {
-        crop: 'auto',
-        gravity: 'auto',
-        width: 500,
-        height: 500,
-    });
-    
-    console.log(autoCropUrl);    
-})();
+// Configuration
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_CLOUD_KEY,
+    api_secret: process.env.CLOUDINARY_CLOUD_SECRET// Click 'View API Keys' above to copy your API secret
+});
+
+const uploadOnCloudinary = async (localFilePath) => {
+    try {
+        if (!localFilePath) return null;
+        const response = await cloudinary.uploader.upload(localFilePath, {
+            resource_type: "auto"
+        })
+        console.log("file have een uploaded", response.url);
+        return response;
+
+    }
+    catch (err) {
+        fs.unlinkSync(localFilePath);
+
+        return null;
+    }
+}
+
+export { uploadOnCloudinary }
