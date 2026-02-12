d82ae3bda7d8c9335f52858c6c60a7b74c17f5c2
Your Nameaymansiddiqui2006 <aymansiddiqui2006@gmail.com>
2026-02-12 23:36:00 +0530

    fix: correct import statement and secure routes with verifyJWT middleware

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,7 +1,10 @@
 import { Router } from "express";
 import { loginUser, logoutUser, registerUser, refreshAccessToken } from "../controllers/user.controller.js";
+import {changeCurrentPassword, getCurrentUser, UserDataChange, UpdateAvatar, UpdateCoverImg} from "../controllers/updates.controller.js"
+import {getUserChannelProfile} from "../controllers/subscription.controller.js"
+import {watchHistory} from "../controllers/watchHistory.controller.js"
 import { upload } from "../middleWare/multer.js";
-import { verfyJWT } from "../middleWare/Auth.middleware.js";
+import {  verifyJWT  } from "../middleWare/Auth.middleware.js";
 
 const router = Router()
 
@@ -20,9 +23,23 @@ router.route("/register").post(
 
 router.route("/login").post(loginUser)
 
-//secureed routes
-router.route("/logout").post(verfyJWT, logoutUser)
+router.route("/logout").post(verifyJWT , logoutUser)
 
 router.route("/refresh-token").post(refreshAccessToken)
+
+router.route("/change-password").patch(verifyJWT , changeCurrentPassword)
+
+router.route("/current-user").get(verifyJWT ,getCurrentUser)
+
+router.route("/update-account").patch(verifyJWT ,UserDataChange)
+
+router.route("/update-avatar").patch(verifyJWT ,upload.single("avatar"),UpdateAvatar)
+
+router.route("/update-cover-image").patch(verifyJWT ,upload.single("coverImage"),UpdateCoverImg)
+
+router.route("/c/:username").get(verifyJWT,getUserChannelProfile)
+
+router.route("/watch-history").get(verifyJWT,watchHistory)
+
 
 export default router
