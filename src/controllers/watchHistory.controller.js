b111b58a7977239b286f68717442f2a0e0d27b56
Your Nameaymansiddiqui2006 <aymansiddiqui2006@gmail.com>
2026-02-12 23:08:31 +0530

    add watchHistory controller to fetch user watch history with video and owner details

---
+++ src/controllers/watchHistory.controller.js
@@ -0,0 +1,54 @@
+import { asyncHandler } from "../utility/asyncHandler.js";
+import { User } from "../models/user.model.js"
+import { ApiResponse } from '../utility/APIresponse.js'
+import mongoose from "mongoose";
+
+const watchHistory = asyncHandler(async (req, res) => {
+    const user = await User.aggregate([
+        {
+            $match: {
+                _id: new mongoose.Types.ObjectId(req.user._id),
+            }
+        }, {
+            $lookup: "videos",
+            localField: "watchHistory",
+            foreignField: "_id",
+            as: "watchHistory",
+            pipeline: [
+                {
+                    $lookup: "users",
+                    localField: "owner",
+                    foreignField: "_id",
+                    as: "owner",
+                    pipeline: [
+                        {
+                            $project: {
+                                Fullname: 1,
+                                username: 1,
+                                avatar: 1,
+                            }
+                        }
+                    ]
+                },
+                {
+                    $addField: {
+                        owner: {
+                            $first: "$owner"
+                        }
+                    }
+                }
+            ]
+        }
+    ])
+    return res.
+        status(200)
+        .json(
+            new ApiResponse(200,
+                user[0], watchHistory
+                , "watch history fetched successfully"
+            )
+        )
+})
+
+export { watchHistory }
+
